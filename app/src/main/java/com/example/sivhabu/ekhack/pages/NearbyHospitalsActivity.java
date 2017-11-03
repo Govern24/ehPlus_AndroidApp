ef33acc0815a527a1cc1213cabb021fb1b5d9332
Govern24 <phulusogovern@gmail.com>
2017-11-04 01:21:02 +0200

    Pages Creation

---
+++ app/src/main/java/com/example/sivhabu/ekhack/pages/NearbyHospitalsActivity.java
@@ -0,0 +1,15 @@
+package com.example.sivhabu.ekhack.pages;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sivhabu.ekhack.R;
+
+public class NearbyHospitalsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nearby_hospitals);
+    }
+}
